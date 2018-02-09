b6cb856e9186ce652657223c82dbcadb4b6849cc
Jean <demeljean@hotmail.fr>
2018-02-09 12:37:11 +0100

    added first dao and entities

--- default/src/routes.php
+++ default/src/routes.php
@@ -2,11 +2,15 @@
 
 use Slim\Http\Request;
 use Slim\Http\Response;
+use simplon\entities\Person;
+use simplon\dao\DaoPerson;
 
 // Routes
-
+phpinfo();
 $app->get('/', function (Request $request, Response $response, array $args) {
-
+    $dao = new DaoPerson();
+    var_dump($dao->getAll());
+    
     // Render index view
     return $this->view->render($response, 'index.twig', [
         'variable' => 'Yes It works'
