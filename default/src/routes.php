7c6e6bbae734000169e1e7f290b39816d4d9b453
matmorel <pub.imagiin@gmail.com>
2018-02-15 15:17:09 +0100

    add comments

--- default/src/routes.php
+++ default/src/routes.php
@@ -41,8 +41,11 @@ $app->post('/addperson', function (Request $request, Response $response, array $
 })->setName('addperson');
 
 $app->get('/updateperson/{id}', function (Request $request, Response $response, array $args) {
+    //On instancie le DAO
     $dao = new DaoPerson;
+    //On récupère la Person à partir de l'id
     $person = $dao->getById($args['id']);
+    // On affiche la vue du formulaire d'update d'une peronne
     return $this->view->render($response, 'updateperson.twig', [
         'person' => $person
     ]);
@@ -50,15 +53,20 @@ $app->get('/updateperson/{id}', function (Request $request, Response $response, 
 })->setName('updateperson');
 
 $app->post('/updateperson/{id}', function (Request $request, Response $response, array $args) {
+    //On instancie le DAO
     $dao = new DaoPerson;
+    //On récupère les données du formulaire
     $postData = $request->getParsedBody();
+    //On récupère la Person à partir de l'id
     $person = $dao->getById($args['id']);
-
+    //On met à jour son nom, sa date de naissance et son genre
     $person->setName($postData['name']);
     $person->setBirthdate(new \DateTime($postData['birthdate']));
     $person->setGender($postData['gender']);
+    //On update la personne
     $dao->update($person);
-
+    //On récupère l'URL da la route index (page d'accueil)
     $redirectUrl = $this->router->pathFor('index');
+    //On redirige l'utilisateur sur la page d'accueil
     return $response->withRedirect($redirectUrl);
 })->setName('updateperson');
